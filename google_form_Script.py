03c31a4503e29ffd5bd7d12c3ced357acde498a5
Muzamil nawaz <mzmlnwz5@gmail.com>
2021-05-20 08:39:51 +0500

    Add files via upload

--- google_form_Script.py
+++ google_form_Script.py
@@ -1,19 +1,77 @@
 import requests
 from selenium import webdriver 
-tinyurl = "INSERT_EXTENSION"
-resp = requests.get("http://tinyurl.com/{}".format(tinyurl))
-# use chrome via driver = webdriver.Chrome() 
-driver = webdriver.Firefox()
+from selenium.webdriver.common.keys import Keys 
+from xlrd import open_workbook
+import time
 
-driver.get(resp.url)
-full_name = "INSERT_FULL_NAME"
-sid = "INSERT_YOUR_SID"
 
-js = """
-document.querySelectorAll("div.quantumWizTogglePaperradioEl.docssharedWizToggleLabeledControl.freebirdThemedRadio.freebirdThemedRadioDarkerDisabled.freebirdFormviewerViewItemsRadioControl")[0].click(); 
-document.querySelectorAll("div.quantumWizTogglePaperradioEl.docssharedWizToggleLabeledControl.freebirdThemedRadio.freebirdThemedRadioDarkerDisabled.freebirdFormviewerViewItemsRadioControl")[2].click(); 
-document.querySelectorAll("input.quantumWizTextinputPaperinputInput")[0].value = "{full_name}";
-document.querySelectorAll("input.quantumWizTextinputPaperinputInput")[1].value = "{sid}";
-document.querySelectorAll("input.quantumWizTextinputPaperinputInput")[2].value = FB_PUBLIC_LOAD_DATA_[1][1][4][4][0][4][0][2][0];
-""".format(full_name=full_name,sid=sid)
-driver.execute_script(js)
+book =  open_workbook("us500.xlsx")
+sheet = book.sheet_by_index(0)
+
+names = []
+emails =[]
+
+for k in range(1,sheet.nrows):
+    names.append(str(sheet.row_values(k)[1-1]))
+    emails.append(str(sheet.row_values(k)[11-1]))
+print(names,emails)
+
+
+option = webdriver.ChromeOptions()
+option.add_argument("-incognito")
+#option.add_argument("--headless")
+#option.add_argument("disable-gpu")
+
+browser = webdriver.Chrome(executable_path='G:\Google form automation\chromedriver_win32\chromedriver.exe', options=option)
+
+browser.get("https://forms.gle/48AG4X42zywV89TL8")
+
+for i in range(12,150):
+    time.sleep(4)
+    browser.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(names[i])
+    browser.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(emails[i])
+    browser.find_element_by_xpath('//*[@id="i18"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i37"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i50"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i63"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i73"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i86"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i111"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i121"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i137"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i156"]/div[3]/div').click()
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span/span').click()
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="i5"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i24"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i37"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i50"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i60"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i67"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i83"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i99"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i109"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i125"]/div[3]/div').click()
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div[2]/span/span').click()
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="i14"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i30"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i40"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i53"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i75"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i85"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i110"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i120"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i142"]/div[3]/div').click()
+    browser.find_element_by_xpath('//*[@id="i152"]/div[3]/div').click()
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div[2]/span/span').click()
+    time.sleep(2)
+    browser.get("https://forms.gle/48AG4X42zywV89TL8")
+
+    
+# js = """
+# document.querySelector('.quantumWizTextinputPaperinputInput exportInput').value = 'adasd'; """
+# browser.execute_script(js)
